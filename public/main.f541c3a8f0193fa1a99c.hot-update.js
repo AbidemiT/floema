8f92b647f615a63ba0155bf6e0c3f8d3a323d7eb
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2022-11-05 15:22:08 +0100

    prismic headless cms setup and implementation

---
+++ public/main.f541c3a8f0193fa1a99c.hot-update.js
@@ -0,0 +1,24 @@
+"use strict";
+self["webpackHotUpdatefloema"]("main",{
+
+/***/ "./app/index.js":
+/*!**********************!*\
+  !*** ./app/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("767b902a553127b72670")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5mNTQxYzNhOGYwMTkzZmExYTk5Yy5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7OztVQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZmxvZW1hL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJzb3VyY2VzQ29udGVudCI6WyJfX3dlYnBhY2tfcmVxdWlyZV9fLmggPSAoKSA9PiAoXCI3NjdiOTAyYTU1MzEyN2I3MjY3MFwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
